aa6ce44cf44a9c6ffa45f81758d21877a6e1650b
Doyeon <archdo0909@gmail.com>
2020-08-24 09:47:30 +0900

    Update button control

---
+++ _ctrl_button.py
@@ -0,0 +1,26 @@
+import os
+import signal
+import subprocess
+import RPi.GPIO as GPIO
+
+run_state_flag = False
+
+def button_callback(channel):
+    if run_state_flag:
+        cmd = subprocess.run(["sudo", "python3", "/home/pi/workspace/run.py"])
+
+        pro = subprocess.Popen()
+
+        run_state_flag = False
+    else:
+        subprocess.run
+    print("button was pushed")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.add_event_detect(6, GPIO.RISING, callback=button_callback)
+
+message = input("Press enter to quit\n\n")
+
+GPIO.cleanup()
---
+++ pro_kill.py
@@ -0,0 +1,13 @@
+import os
+import signal
+import subprocess
+
+try :
+    cmd = subprocess.run(["sudo", "python3", "/home/pi/workspace/run.py"])
+
+    pro = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                           shell=True, preexec_fn=os.setsid)
+    mes = input("quit\n\n")
+except KeyboardInterrupt:
+
+    os.killpg(os.getpid(pro.pid), signal.SIGTERM)
