3a230346e09512144c57e4075986a11879276499
Doyeon <archdo0909@gmail.com>
2020-08-24 15:11:52 +0900

    Fix script doesn't play wav file

--- _ctrl_boot_up_sound.py
+++ _ctrl_boot_up_sound.py
@@ -1,6 +1,11 @@
 import sounddevice as sd
 import soundfile as sf
 
-data, fs = sf.read('/home/pi/rpi-daemon-py/sound/beep.wav', dtype="float32")
-sd.play(data, fs)
+def play_beep():
+    data, fs = sf.read('/home/pi/rpi-daemon-py/sound/beep.wav', dtype="float32")
+    sd.play(data, fs)
+    sd.wait()
+
+if __name__ == "__main__":
+    play_beep()
 
