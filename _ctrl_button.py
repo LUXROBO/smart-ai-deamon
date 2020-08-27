eeccbf40e4c41d314eedbd83c67a545a6e9d398e
Doyeon <archdo0909@gmail.com>
2020-08-27 17:21:21 +0900

    Update _ctrl_button to class

--- _ctrl_button.py
+++ _ctrl_button.py
@@ -1,22 +1,38 @@
 import os
+import time
 import signal
 import subprocess
+
 import RPi.GPIO as GPIO
-import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+class CtrlFan:
+    _instance = None
+    
+    @classmethod
+    def _genInstance(self):
+        return cls._instance
 
-run_flag = False
+    @classmethod
+    def instance(cls, *args, **kargs):
+        cls._instance = cls(*args, **kargs)
+        cls._instance = _getInstance
+        return cls._instance
 
-while True:
-    pin = GPIO.wait_for_edge(6, GPIO.FALLING, timeout=1000)
-    if pin is None:
-        print("timeout")
-    else:
-        print("edge detected")
-        if run_flag:
-            pass
-        else:
-            cmd = subprocess.check_output(["sudo", "/usr/bin/python3", "/home/pi/workspace/run.py"])
-            run_flag = False
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+        self.run_flag = False
+
+    def run(self):
+        while True:
+            pin = GPIO.wait_for_edge(6, GPIO.FALLING, timeout=1000)
+            if pin is None:
+                print("timeout")
+            else:
+                print("edge detected")
+                if run_flag:
+                    pass
+                else:
+                    cmd = subprocess.check_output(["sudo", "/usr/bin/python3", "/home/pi/workspace/run.py"])
+                    run_flag = False
