ad2690c0ffa2982edbd875abda1ae5e663f35182
Doyeon <archdo0909@gmail.com>
2020-08-31 10:39:18 +0900

    Update unused file and sorting folders

--- _ctrl_button.py
+++ _ctrl_button.py
@@ -15,15 +15,15 @@ pid = None
 while True:
     pin = GPIO.wait_for_edge(6, GPIO.FALLING, timeout=1000)
     if pin is None:
-        print("timeout")
+        #print("timeout")
+        pass
     else:
-        print("edge detected")
+        #print("edge detected")
         if run_flag:
             os.killpg(pid, signal.SIGTERM)
             run_flag = False
         else:
             p = subprocess.Popen(['sudo', '/usr/bin/python3', '/home/pi/workspace/run.py'],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=False,preexec_fn=os.setsid)
-            print(p.pid)
             pid = p.pid
             time.sleep(3)
             run_flag = True
